71c559be0f7d94d2eea4b28219d903bad14a5add
notlega <onghinjourn@gmail.com>
2022-09-07 23:10:14 +0800

    refactor: :recycle: Updates formatting

--- Practical3/Q3.py
+++ Practical3/Q3.py
@@ -11,47 +11,47 @@
 
 
 def main():
-    print("----------------------------------------------------")
-    booleanExit = False
-    while booleanExit == False:
+		print("----------------------------------------------------")
+		booleanExit = False
+		while booleanExit == False:
 
-        # number input in integer
-        userInput = int(input("Enter a number between 0 - 100: "))
+				# number input in integer
+				userInput = int(input("Enter a number between 0 - 100: "))
 
-        if userInput > 100 or userInput < 0:
-            # if the number is not between 0 - 100
-            print("Invalid input")
-        else:
-            # if the number is between 0 - 100
+				if userInput > 100 or userInput < 0:
+						# if the number is not between 0 - 100
+						print("Invalid input")
+				else:
+						# if the number is between 0 - 100
 
-            if userInput >= 80:
-                # if the number is 80 - 100
-                print("Your grade is A!")
-            elif userInput >= 75:
-                # if the number is 75 - 79
-                print("Your grade is B+!")
-            elif userInput >= 70:
-                # if the number is 70 - 74
-                print("Your grade is B!")
-            elif userInput >= 65:
-                # if the number is 65 - 69
-                print("Your grade is C+!")
-            elif userInput >= 60:
-                # if the number is 60 - 64
-                print("Your grade is C!")
-            elif userInput >= 50:
-                # if the number is 50 - 59
-                print("Your grade is D!")
-            elif userInput >= 40:
-                # if the number is 40 - 49
-                print("Your grade is E!")
-            else:
-                # if the number is 0 - 39
-                print("Your grade is F!")
+						if userInput >= 80:
+								# if the number is 80 - 100
+								print("Your grade is A!")
+						elif userInput >= 75:
+								# if the number is 75 - 79
+								print("Your grade is B+!")
+						elif userInput >= 70:
+								# if the number is 70 - 74
+								print("Your grade is B!")
+						elif userInput >= 65:
+								# if the number is 65 - 69
+								print("Your grade is C+!")
+						elif userInput >= 60:
+								# if the number is 60 - 64
+								print("Your grade is C!")
+						elif userInput >= 50:
+								# if the number is 50 - 59
+								print("Your grade is D!")
+						elif userInput >= 40:
+								# if the number is 40 - 49
+								print("Your grade is E!")
+						else:
+								# if the number is 0 - 39
+								print("Your grade is F!")
 
-            # exit the program
-            booleanExit = True
+						# exit the program
+						booleanExit = True
 
 
 if __name__ == "__main__":
-    main()
+		main()
