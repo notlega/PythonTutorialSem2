77f93b53e8740b32c7b444e96e44ae65734bee32
notlega <onghinjourn@gmail.com>
2022-09-08 10:34:37 +0800

    feat: :sparkles: Created initial classes for Assignment

---
+++ Assignment/classes/Dessert.py
@@ -0,0 +1,20 @@
+from classes.Food import Food
+
+
+class Dessert(Food):
+		"""
+		A child class of Food used to represent types of Desserts
+
+		...
+
+		Attributes
+		----------
+		NULL
+
+		Methods
+		-------
+		NULL
+		"""
+
+		def __init__(self) -> None:
+				super().__init__()
---
+++ Assignment/classes/Food.py
@@ -0,0 +1,22 @@
+class Food:
+		"""
+		A class used to represent types of Food
+
+		...
+
+		Attributes
+		----------
+		NULL
+
+		Methods
+		-------
+		NULL
+		"""
+
+		def __init__(self) -> None:
+				"""
+				Parameters
+				----------
+				NULL
+				"""
+				pass
